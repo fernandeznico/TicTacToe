6b0655d4f4d88495d5b370fdccef47fe05f3a31a
fernandeznico <nicofernadnez@alumnos.unc.edu.ar>
2019-09-24 12:17:05 -0300

    Add:  + move (function) complete
    Signed-off-by: fernandeznico <nicofernadnez@alumnos.unc.edu.ar>

--- BackEnd/ComputerIA.py
+++ BackEnd/ComputerIA.py
@@ -2,7 +2,8 @@ from typing import Set
 
 from BackEnd.StaticDataBase import win_sets, center_position
 from BackEnd.Generals import return_first_difference_of_one_in_sets_or_none as get_win_position, \
-    return_list_of_set_which_no_have_objects_present_in_set as filter_no_possible_win_sets
+    return_list_of_set_which_no_have_objects_present_in_set as filter_no_possible_win_sets, \
+    return_one_object_in_a_set_present_in_difference_with_one_of_the_list_where_difference_size_is_lower as general_move
 
 
 class ComputerIA:
@@ -19,7 +20,13 @@ class ComputerIA:
         # Try take center position
         if center_position not in self.positions_taken.union(self.rival_positions_taken):
             return center_position
-        # @continue
+        # Take a second place in line or any place
+        # @TODO here can make better plays:
+        available_win_sets = filter_no_possible_win_sets(self.rival_positions_taken, win_sets)
+        position_to_take = general_move(win_sets, self.positions_taken)
+        if position_to_take:
+            return position_to_take
+        raise Exception('Are needed available positions to play')
 
     def try_move_for(self, intention: str) -> str:
         if intention == 'win':
