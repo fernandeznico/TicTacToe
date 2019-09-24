9af9755f5ae81b02274c1f1c6dc7d63a1f016f7e
fernandeznico <nicofenane@gmail.com>
2019-09-24 11:38:47 -0300

    Add:  + TEST return_list_of_set_which_no_have_objects_present_in_set (function)

--- BackEnd/UnitTests.py
+++ BackEnd/UnitTests.py
@@ -6,6 +6,10 @@ class GeneralsTest(unittest.TestCase):
         from BackEnd.Generals import return_first_difference_of_one_in_sets_or_none
         self.assertEqual(return_first_difference_of_one_in_sets_or_none([{'a'}], [{'b'}]), 'a')
 
+    def test_return_list_of_set_which_no_have_objects_present_in_set(self):
+        from BackEnd.Generals import return_list_of_set_which_no_have_objects_present_in_set
+        self.assertEqual(return_list_of_set_which_no_have_objects_present_in_set({'a'}, [{'a'}, {'b'}]), [{'b'}])
+
 
 if __name__ == '__main__':
     unittest.main()
