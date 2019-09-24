1706fc63c1c4205301e0ab3cbcabaf2cd6f0443a
fernandeznico <nicofernadnez@alumnos.unc.edu.ar>
2019-09-24 10:53:48 -0300

    Fix:  + Player have only one set of positions in possession

--- BackEnd/ComputerIA.py
+++ BackEnd/ComputerIA.py
@@ -6,14 +6,26 @@ from BackEnd.Generals import return_first_difference_of_one_in_sets_or_none as g
 
 class ComputerIA:
     @staticmethod
-    def try_win(positions_taken: List[Set[str]]):
+    def try_win(positions_taken: Set[str]) -> str:
         """
         If can win, then win.
         Check if have almost one win set from need only one.
-        :return: position to take or None
+        :param positions_taken: set of positions in computer possession
+        :return: position to take for win or ''
         """
-        return get_win_position(win_sets, positions_taken)
+        return get_win_position(win_sets, [positions_taken])
 
     @staticmethod
-    def move():
-        try_win()
+    def try_defend(rival_positions_taken: List[Set[str]]) -> str:
+        """
+        If need cover a position, to prevent rival win, then cover.
+        :param rival_positions_taken:
+        :return:
+        """
+
+    @staticmethod
+    def move(positions_taken: List[Set[str]]):
+        take_position = ComputerIA.try_win(positions_taken)
+        if take_position:
+            return take_position
+
