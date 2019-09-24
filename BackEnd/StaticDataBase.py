b6c13f194b4aa00ce3ccf499093d2e221e51cf7e
fernandeznico <nicofenane@gmail.com>
2019-09-24 11:22:46 -0300

    Add:  + center_position (str)

--- BackEnd/StaticDataBase.py
+++ BackEnd/StaticDataBase.py
@@ -9,3 +9,4 @@ g | h | i
 
 win_sets = [{'a', 'b', 'c'}, {'d', 'e', 'f'}, {'g', 'h', 'i'}, {'a', 'd', 'g'}, {'b', 'e', 'h'}, {'c', 'f', 'i'},
             {'a', 'e', 'i'}, {'c', 'e', 'g'}]
+center_position = 'e'
