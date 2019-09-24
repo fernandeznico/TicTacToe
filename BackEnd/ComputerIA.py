dd96ba13b49bc400b948f38465a2e5386cbf708a
fernandeznico <nicofernadnez@alumnos.unc.edu.ar>
2019-09-24 11:09:09 -0300

    Modify:  + Same result, better code

--- BackEnd/ComputerIA.py
+++ BackEnd/ComputerIA.py
@@ -1,31 +1,25 @@
-from typing import List, Set
+from typing import Set
 
 from BackEnd.StaticDataBase import win_sets
 from BackEnd.Generals import return_first_difference_of_one_in_sets_or_none as get_win_position
 
 
 class ComputerIA:
-    @staticmethod
-    def try_win(positions_taken: Set[str]) -> str:
-        """
-        If can win, then win.
-        Check if have almost one win set from need only one.
-        :param positions_taken: set of positions in computer possession
-        :return: position to take for win or ''
-        """
-        return get_win_position(win_sets, [positions_taken])
+    def __init__(self, positions_taken: Set[str], rival_positions_taken: Set[str]):
+        self.positions_taken = positions_taken
+        self.rival_positions_taken = rival_positions_taken
 
-    @staticmethod
-    def try_defend(rival_positions_taken: List[Set[str]]) -> str:
-        """
-        If need cover a position, to prevent rival win, then cover.
-        :param rival_positions_taken:
-        :return:
-        """
+    def try_move_for(self, intention: str):
+        if intention == 'win':
+            positions_set = self.positions_taken
+        elif intention == 'defend':
+            positions_set = self.rival_positions_taken
+        else:
+            raise ValueError('intention need be "win" or "defend" not "%s"' % intention)
+        take_position = get_win_position(win_sets, [positions_set])
+        return take_position
 
-    @staticmethod
-    def move(positions_taken: List[Set[str]]):
-        take_position = ComputerIA.try_win(positions_taken)
-        if take_position:
-            return take_position
+    def move(self):
+        self.try_move_for('win')
+        self.try_move_for('defend')
 
