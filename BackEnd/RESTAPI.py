333841cfa3ba50bcdf5b513c6d0e586868ec7a99
Nicolás Fernández <nicofernandez@alumnos.unc.edu.ar>
2019-10-03 17:29:26 -0300

    Se comprueba interaciión con la API desde JS.

--- BackEnd/RESTAPI.py
+++ BackEnd/RESTAPI.py
@@ -2,19 +2,22 @@
 @package RESTAPI
 RESTful implementation for a backend support to a TicTacToe game.
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, json
+from flask_cors import CORS
 
 from BackEnd.ComputerIA import ComputerIA
 
 app = Flask(__name__)
+app.config.from_object(__name__)
 
+# enable CORS
+CORS(app, resources={r'/*': {'origins': '*'}})
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET'])
 def get_computer_move():
-    request_json = request.json
-    cia = ComputerIA(set(request_json['computer']), set(request_json['player']))
+    request_args = request.args
+    request_dict = request_args.to_dict(flat=False)
+    if not request_dict:
+        return jsonify({'error': 'Need player positions'})
+    cia = ComputerIA(set(request_dict['computer[]']), set(request_dict['player[]']))
     return jsonify(cia.move())
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
---
+++ __main__.py
@@ -0,0 +1,5 @@
+from BackEnd import RESTAPI
+
+
+if __name__ == "__main__":
+    RESTAPI.app.run(debug=True)
