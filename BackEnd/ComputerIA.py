b87423e424e029228b4de34d98583c3f5efa511d
Nicolás Fernández <nicofernandez@alumnos.unc.edu.ar>
2019-10-04 02:47:55 -0300

    Add case no have evident move

--- BackEnd/ComputerIA.py
+++ BackEnd/ComputerIA.py
@@ -1,6 +1,6 @@
 from typing import Set
 
-from BackEnd.StaticDataBase import win_sets, center_position
+from BackEnd.StaticDataBase import win_sets, center_position, places
 from BackEnd.Generals import return_first_difference_of_one_in_sets_or_none as get_win_position, \
     return_list_of_set_which_no_have_objects_present_in_set as filter_no_possible_win_sets, \
     return_one_object_in_a_set_present_in_difference_with_one_of_the_list_where_difference_size_is_lower as general_move
@@ -13,19 +13,23 @@ class ComputerIA:
 
     def move(self) -> str:
         types_of_movements = ('win', 'defend')
+        # First wins if is possible, second defends if is needed:
         for move_type in types_of_movements:
             position_to_take = self.try_move_for(move_type)
             if position_to_take:
                 return position_to_take
-        # Try take center position
+        # Try take center position:
         if center_position not in self.positions_taken.union(self.rival_positions_taken):
             return center_position
-        # Take a second place in line or any place
+        # Take a second place in line or any place:
         # @TODO here can make better plays:
         available_win_sets = filter_no_possible_win_sets(self.rival_positions_taken, win_sets)
-        position_to_take = general_move(win_sets, self.positions_taken)
-        if position_to_take:
-            return position_to_take
+        print(available_win_sets)
+        position_to_take = general_move(available_win_sets, self.positions_taken)
+        # Take any:
+        available_positions = places - self.positions_taken - self.rival_positions_taken
+        if available_positions:
+            return available_positions.pop()
         raise Exception('Are needed available positions to play')
 
     def try_move_for(self, intention: str) -> str:
